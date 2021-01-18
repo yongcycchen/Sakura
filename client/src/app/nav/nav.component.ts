4482264861175140448a91e1688b2fed01a88383
yongcycchen <yong.cycchen@gmail.com>
2021-01-18 14:48:21 -0800

    section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -18,9 +18,6 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe(response=>{
       //console.log(response);
       this.router.navigateByUrl('/members');
-    },error=>{
-      console.log(error);
-      this.toastr.error(error.error);
     })
   }
   logout(){
