1eddcc26e1d260d860c71802fb698bcaa5605397
yongcycchen <yong.cycchen@gmail.com>
2021-01-25 22:41:38 -0800

    Reactive forms

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,18 +4,15 @@ import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { from } from 'rxjs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
@@ -27,6 +24,8 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { DateInputComponent } from './_forms/date-input/date-input.component';
+// import { TextInputComponent } from './_forms/text-input/text-input.component';
 
 @NgModule({
   declarations: [
@@ -43,7 +42,9 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     ServerErrorComponent,
     MemberCardComponent,
     MemberEditComponent,
-    PhotoEditorComponent
+    PhotoEditorComponent,
+    DateInputComponent,
+    // TextInputComponent
   ],
   imports: [
     BrowserModule,
@@ -51,6 +52,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule,
     MatProgressSpinnerModule
     // NgxSpinnerModule,
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -9,19 +12,74 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any ={};
-  constructor(private accountService:AccountService, private toastr: ToastrService) { }
+  registerForm: FormGroup={}as FormGroup;
+  bsConfig : Partial<BsDatepickerConfig>={};
+  maxDate: Date={}as Date;
+  validationErrors:string[]=[];
+  
+  constructor(private accountService:AccountService, private toastr: ToastrService,
+     private fb:FormBuilder, private router:Router) {
+    this.bsConfig = {
+      containerClass:'theme-red',
+      dateInputFormat:'MM/DD/YYYY'
+    }
+    this.maxDate = new Date();
+  }
 
   ngOnInit(): void {
+    this.intitializeForm();
+  }
+
+  intitializeForm(){
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['',Validators.required],
+      knownAs: ['',Validators.required],
+      dateOfBirth: ['',Validators.required],
+      city: ['',Validators.required],
+      country: ['',Validators.required],
+        password: ['',[Validators.required,
+            Validators.minLength(4),Validators.maxLength(8)]],
+        confirmPassword: ['',[Validators.required]]}
+        ,{validator: this.MustMatch('password','confirmPassword')})
+    // this.registerForm = new FormGroup({
+    //   username: new FormControl('hello',Validators.required),
+    //   password: new FormControl('',[Validators.required,
+    //       Validators.minLength(4),Validators.maxLength(8)]),
+    //   confirmPassword: new FormControl('',[Validators.required, this.matchValues('password')])})
+  }
+
+  // matchValues(matchTo:string):ValidatorFn{
+  //   return (control:AbstractControl) =>{
+  //     return control?.value === control?.parent?.controls[matchTo].value 
+  //     ? null : {notSame:true}
+  //   }
+  // }
+
+  MustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+        const control = formGroup.controls[controlName];
+        const matchingControl = formGroup.controls[matchingControlName];
+
+        if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+            // return if another validator has already found an error on the matchingControl
+            return;
+        }
+
+        // set error on matchingControl if validation fails
+        if (control.value !== matchingControl.value) {
+            matchingControl.setErrors({ notSame: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+    }
   }
 
   register(){
-    this.accountService.register(this.model).subscribe(response=>{
-      console.log(response);
-      this.cancel();
+    this.accountService.register(this.registerForm.value).subscribe(response=>{
+      this.router.navigateByUrl('/members');
     },error=>{
-      console.log(error);
-      this.toastr.error(error.error);
+      this.validationErrors = error;
     })
   }
 
